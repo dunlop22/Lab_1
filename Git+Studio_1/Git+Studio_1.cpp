69bf10326d578b6977164d08dacebb66ad4d605e
Egor Golikov <20020110ll@gmail.com>
2021-09-06 16:42:20 +0700

    Добавлена функция находения arcsin(x)

--- Git+Studio_1/Git+Studio_1.cpp
+++ Git+Studio_1/Git+Studio_1.cpp
@@ -4,7 +4,24 @@
 #include <locale.h>
 using namespace std;
 
-#include <iostream>
+//arcsin(x)
+double func1(double x, double e)
+{
+    double sum, tek;
+    int k;
+    k = 0;
+    tek = x;
+    sum = 0;
+
+    while (fabs(tek) > e)
+    {
+        sum = sum + tek;
+        k++;
+        tek = tek * x * x * (2 * k - 1) * (2 * k - 1) / (2 * k) / (2 * k + 1);
+    }
+    return sum;
+}
+
 
 int main()
 {
